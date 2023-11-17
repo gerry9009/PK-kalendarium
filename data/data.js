adb5f0f509b432dd89dbb4dab3a0dc84d71753b2
Gergo Buglyo <buglyogergo@gmail.com>
2023-11-17 22:20:02 +0100

    data file frissítése

--- data/data.js
+++ data/data.js
@@ -4,116 +4,139 @@ const days = [
     link: "https://www.youtube.com/embed/EDc38uE0bCw?si=u9RnCUoKXxpDdtNZ",
     isFlipped: false,
   },
+
   {
     day: 2,
-    link: "https://www.youtube.com/embed/4vxClaxJUhw?si=7lutcnQeqhSFHGqe",
+    link: "https://www.youtube.com/embed/7NBVJMqmifg?si=J9RG5fva-kqIOWnc",
     isFlipped: false,
   },
+
   {
     day: 3,
-    link: "https://www.youtube.com/embed/It_o6lXfLrc?si=hwrkoBuaK5isdh-7",
-    isFlipped: false,
-  },
-  {
-    day: 4,
-    link: "https://www.youtube.com/embed/R8o8_vpgEpw?si=AAdZocdVfFryfKv_",
-    isFlipped: false,
-  },
-  {
-    day: 5,
-    link: "https://www.youtube.com/embed/qvXvlwust34?si=T4a8IRa-WGqMTL5n",
-    isFlipped: false,
-  },
-  {
-    day: 6,
-    link: "https://www.youtube.com/embed/shAV0U3AZaE?si=f12DqftHodAPKHrb",
-    isFlipped: false,
-  },
-  {
-    day: 7,
-    link: "https://www.youtube.com/embed/8LNmD0F1_xU?si=CNEe64EWQc1PVc3R",
-    isFlipped: false,
-  },
-  {
-    day: 8,
-    link: "https://www.youtube.com/embed/lNuiEcWfPSA?si=5ZNsbteDAqxwh9va",
-    isFlipped: false,
-  },
-  {
-    day: 9,
     link: "https://www.youtube.com/embed/ZI4x_BMM6D8?si=AGJ8kZ14ZyCJaLX7",
     isFlipped: false,
   },
+
+  {
+    day: 4,
+    link: "https://www.youtube.com/embed/80Klug7Dge4?si=LmnhiTVvaCzkhELF",
+    isFlipped: false,
+  },
+
+  {
+    day: 5,
+    link: "https://www.youtube.com/watch?v=5QM-z17n4S4&t=1s",
+    isFlipped: false,
+  },
+
+  {
+    day: 6,
+    link: "https://www.youtube.com/embed/-9kLQxWY-uE?si=1Msu4eGpypqpzeYj",
+    isFlipped: false,
+  },
+
+  {
+    day: 7,
+    link: "https://www.youtube.com/embed/It_o6lXfLrc?si=hwrkoBuaK5isdh-7",
+    isFlipped: false,
+  },
+
+  {
+    day: 8,
+    link: "https://www.youtube.com/embed/8LNmD0F1_xU?si=CNEe64EWQc1PVc3R",
+    isFlipped: false,
+  },
+
+  {
+    day: 9,
+    link: "https://www.youtube.com/embed/R8o8_vpgEpw?si=AAdZocdVfFryfKv_",
+    isFlipped: false,
+  },
+
   {
     day: 10,
     link: "https://www.youtube.com/embed/UePixUhUYqo?si=4ML3QNN_3oIyYoKG",
     isFlipped: false,
   },
+
   {
     day: 11,
+    link: "https://www.youtube.com/embed/lNuiEcWfPSA?si=5ZNsbteDAqxwh9va",
+    isFlipped: false,
+  },
+
+  {
+    day: 12,
+    link: "https://www.youtube.com/embed/qvXvlwust34?si=T4a8IRa-WGqMTL5n",
+    isFlipped: false,
+  },
+
+  {
+    day: 13,
     link: "https://www.youtube.com/embed/IF-VW1vhMuc?si=HlcA7RKD6Twd8zM4",
     isFlipped: false,
   },
+
   {
-    day: 12,
+    day: 14,
     link: "https://www.youtube.com/embed/KlsflpBBkrk?si=3P9aFXWOQcgbM8R0",
     isFlipped: false,
   },
+
   {
-    day: 13,
+    day: 15,
     link: "https://www.youtube.com/embed/TnFzoTGMzwg?si=_ozhXj1VFYSnNszb",
     isFlipped: false,
   },
+
   {
-    day: 14,
-    link: "https://www.youtube.com/embed/_lbCmhiYMEc?si=AUTzXHCnNeDORVNw",
-    isFlipped: false,
-  },
-  {
-    day: 15,
+    day: 16,
     link: "https://www.youtube.com/embed/Qmmtu0HuorQ?si=hOCRLDMZC5HRAOZL",
     isFlipped: false,
   },
+
   {
-    day: 16,
+    day: 17,
+    link: "https://www.youtube.com/embed/_lbCmhiYMEc?si=AUTzXHCnNeDORVNw",
+    isFlipped: false,
+  },
+
+  {
+    day: 18,
     link: "https://www.youtube.com/embed/CRkwNILOtAs?si=GhaX55QtJgm49iV7",
     isFlipped: false,
   },
+
   {
-    day: 17,
-    link: "https://www.youtube.com/embed/80Klug7Dge4?si=LmnhiTVvaCzkhELF",
-    isFlipped: false,
-  },
-  {
-    day: 18,
+    day: 19,
     link: "https://www.youtube.com/embed/qtN7dTId7sc?si=MSbeR3Bv5TD0RHcf",
     isFlipped: false,
   },
+
   {
-    day: 19,
+    day: 20,
     link: "https://www.youtube.com/embed/PJY5tvgSy5U?si=RpD-DhQngWZ1YmGg",
     isFlipped: false,
   },
+
   {
-    day: 20,
+    day: 21,
     link: "https://www.youtube.com/embed/sFbbmHeZgVU?si=6rENHhheEViipIfN",
     isFlipped: false,
   },
-  {
-    day: 21,
-    link: "https://www.youtube.com/embed/FDkM7ODljUs?si=XTGRvkdPOL-lsNYH",
-    isFlipped: false,
-  },
+
   {
     day: 22,
+    link: "https://www.youtube.com/embed/rmqKLiSkKrw?si=v0qOlDC_eig0hXay",
+    isFlipped: false,
+  },
+
+  {
+    day: 23,
     link: "https://www.youtube.com/embed/sFbbmHeZgVU?si=d3JhpsOB01PRknbo",
     isFlipped: false,
   },
-  {
-    day: 23,
-    link: "https://www.youtube.com/embed/-9kLQxWY-uE?si=1Msu4eGpypqpzeYj",
-    isFlipped: false,
-  },
+
   {
     day: 24,
     link: "https://www.youtube.com/embed/0IEOzOpSA2k?si=E12HLxVR4u6krsK5",
