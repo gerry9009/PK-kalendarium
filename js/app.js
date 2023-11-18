ba2f2aaa1ddc2d9959ddd3dd51e825c5206d99f7
Gergo Buglyo <buglyogergo@gmail.com>
2023-11-18 02:02:16 +0100

    JS második rész

--- js/app.js
+++ js/app.js
@@ -1,41 +1,93 @@
 import days from "../data/data.js";
 
 const $container = document.querySelector(".js-container");
+const date = new Date();
+const today = date.getDate();
 
-function templateCard(day) {
+let data;
+
+function saveToLocalStorage() {
+  localStorage.setItem("data", JSON.stringify(data));
+}
+
+function initialData() {
+  const storageData = localStorage.getItem("data");
+  const localData = JSON.parse(storageData);
+
+  if (localData) {
+    data = localData;
+  } else {
+    data = days;
+  }
+}
+
+function templateCard(item) {
   const card = document.createElement("div");
   const front = document.createElement("div");
   const back = document.createElement("div");
 
   card.classList = "card";
-  front.classList = "card_content card_front";
-  back.classList = "card_content card_back is-flipped";
 
-  front.innerHTML = `<h2>December ${day}.</h2>`;
-
-  if (day === 24) {
-    back.innerHTML = `<p>Ma van Karácsony napja</p>`;
+  if (item.isFlipped) {
+    front.classList = "card_content card_front is-flipped";
+    back.classList = "card_content card_back";
   } else {
-    back.innerHTML = `<p>Már csak ${24 - day} nap van Karácsonyig</p>`;
+    front.classList = "card_content card_front";
+    back.classList = "card_content card_back is-flipped";
   }
+
+  front.innerHTML = `<h2>December ${item.day}.</h2>`;
+
+  let message;
+
+  if (Number(item.day) === 24) {
+    message = "Ma van Karácsony napja";
+  } else {
+    message = `Már csak ${24 - item.day} nap van Karácsonyig`;
+  }
+
+  back.innerHTML = `
+    <div class="card_header">
+        <iframe
+            src="${item.link}"
+            title="YouTube video player"
+            frameborder="0"
+            allowfullscreen
+        ></iframe>
+    </div>
+    <div class="card_body">
+        <p>${message}</p>
+    </div>
+  `;
 
   card.appendChild(front);
   card.appendChild(back);
 
   card.addEventListener("click", () => {
-    front.classList.toggle("is-flipped");
-    back.classList.toggle("is-flipped");
+    if (item.day <= today) {
+      front.classList.add("is-flipped");
+      back.classList.remove("is-flipped");
+
+      data[item.day - 1].isFlipped = true;
+
+      saveToLocalStorage();
+    } else {
+      alert(
+        "Hooooo-hooooo-hoooo! Ennek a dátumnak még nincs itt az ideje! Várj türelemmel!"
+      );
+    }
   });
 
   return card;
 }
 
 function render() {
-  for (let i = 1; i <= 24; i++) {
-    const newCard = templateCard(i);
+  for (let day of data) {
+    const newCard = templateCard(day);
 
     $container.appendChild(newCard);
   }
 }
 
+initialData();
 render();
